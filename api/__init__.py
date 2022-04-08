9718c8c9b3641e1cbf40d436ea8ccf3615cbcfbb
root <root@ip-172-31-38-226.ap-south-1.compute.internal>
2022-04-08 05:56:21 +0000

    Bulk Commit

---
+++ api/__init__.py
@@ -0,0 +1,56 @@
+import logging
+import time
+
+from flask import jsonify
+from flask_restful import Api, Resource
+
+import config
+import tasks
+
+LOGGER = logging.getLogger()
+
+api = Api(prefix=config.API_PREFIX)
+
+
+class TaskStatusAPI(Resource):
+    @staticmethod
+    def get(task_id):
+        task = tasks.celery.AsyncResult(task_id)
+        LOGGER.info("TaskStatusAPI")
+        LOGGER.info("Task: %s", task)
+        if task.state == 'PENDING':
+            time.sleep(1)
+            response = {
+                'queue_state': task.state,
+                'status': 'Process is ongoing...',
+            }
+        elif task.state == 'SUCCESS':
+
+            response = {
+                'queue_state': task.state,
+                'result': task.wait()
+            }
+        else:
+            response = {
+                'queue_state': task.state,
+                'result': task.wait()
+            }
+
+        return jsonify(response)
+
+
+class DataProcessingAPI(Resource):
+    @staticmethod
+    def post():
+        LOGGER.info("DataProcessingAPI")
+        task = tasks.process_data.delay()
+
+        LOGGER.info("Task: %s", task)
+
+        return {'task_id': task.id}, 200
+
+
+# data processing endpoint
+api.add_resource(DataProcessingAPI, '/process_data')
+# task status endpoint
+api.add_resource(TaskStatusAPI, '/tasks/<string:task_id>')
---
+++ tasks/__init__.py
@@ -0,0 +1,24 @@
+import logging
+import time
+import random
+
+from celery import Celery
+import config
+
+LOGGER = logging.getLogger()
+
+
+def make_celery():
+    celery = Celery(__name__, broker=config.CELERY_BROKER)
+    celery.conf.update(config.as_dict())
+    return celery
+
+
+celery = make_celery()
+
+
+@celery.task(bind=True)
+def process_data(self):
+    LOGGER.info("Process data: %s", self)
+    time.sleep(30*random.random())
+    return {"succes": "ok"}
